67c8b0abfe3f4f8f06f8e9b56494818962cc6b38
lu-css <dossantossilvalucas963@gmail.com>
2024-01-11 07:11:36 -0300

    [feat] Add reflection to add xml documentation

---
+++ Guardian/Guardian.Application/src/Application.cs
@@ -0,0 +1,8 @@
+namespace Guardian.Application;
+
+/// <summary>
+/// This class is only used for Reflection.
+/// Please do not change anything here
+/// </summary>
+public class Application;
+
--- Guardian/Guardian.UI/Program.cs
+++ Guardian/Guardian.UI/Program.cs
@@ -1,4 +1,5 @@
 using Guardian.DependencyInjection;
+using Guardian.UI.Configurations;
 using Guardian.UI.Controllers.Middlewares.ErrorHandlers;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,37 +9,11 @@ builder.Services
 
 builder.Services.AddControllers();
 
-builder.Services
-    .AddEndpointsApiExplorer()
-    .AddSwaggerGen(c =>
-    {
-        c.CustomSchemaIds(type => type.ToString());
-
-        var assemblies = new List<string>() { "Arq.UI", "Arq.Application" };
-
-        var baseDir = System.AppContext.BaseDirectory;
-
-        foreach (var ass in assemblies)
-        {
-            var path = Path.Combine(System.AppContext.BaseDirectory, $"{ass}.xml");
-
-            c.IncludeXmlComments(path);
-        }
-    });
+builder.Services.AddOpenAPI();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-
-    app.UseReDoc(options =>
-    {
-        options.DocumentTitle = "Endpoints documentation";
-        options.SpecUrl = "/swagger/v1/swagger.json";
-    });
-}
+app.UseOpenAPI();
 
 app.UseHttpsRedirection();
 
